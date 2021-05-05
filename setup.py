a63341bd7081e69b392ad5c0e734a6182e7c98d5
abetraoui <abel.betraoui@biomerieux.com>
2021-05-05 18:40:30 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import setuptools
+from distutils.core import setup
+
+
+with open('README.md', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="python_db_lecture",
+    version='1.0.0',
+    description='',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='abetraou',
+    packages=['python_db_lecture'],
+    install_requires=['numpy==1.20.2', 'pandas==1.2.4'],
+    extras_require={
+        "notebook": ['jupyter', 'matplotlib', 'seaborn']
+    }
+)
